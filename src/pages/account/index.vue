00ebb5b6428eddeae4ddb522afdec30f77b4c19c
liuwei <wei.liu@pingxx.com>
2023-08-10 17:27:32 +0800

    m:双击登录

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -4,18 +4,13 @@
       <div v-for="vibe in data" :key="vibe.vibe">
         <div class="vibe-title">{{ vibe.vibe }}</div>
         <div class="vibe-data">
-          <el-table :data="vibe.data">
+          <el-table :data="vibe.data" @row-dblclick="handleLogin">
             <el-table-column prop="account"></el-table-column>
             <el-table-column prop="pwd"></el-table-column>
             <el-table-column
               prop="desc"
               show-overflow-tooltip
             ></el-table-column>
-            <el-table-column prop="action" align="center" width="100px">
-              <template #default="scope">
-                <el-icon @click="handleLogin(scope.row)"><Promotion /></el-icon>
-              </template>
-            </el-table-column>
           </el-table>
         </div>
       </div>
