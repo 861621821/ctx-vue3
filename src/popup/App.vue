62f567d81bf64cf1a13116d2ca55661ea49f599a
liuwei <wei.liu@pingxx.com>
2023-12-13 14:34:30 +0800

    public init

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,81 +1,18 @@
 <template>
-  <div class="side-bar-container">
-    <el-badge :is-dot="newTodo">
-      <img src="@/assets/image/logo.png" class="logo" alt="" srcset="" />
-    </el-badge>
-    <router-link :to="menu.path" v-for="menu in menus" :key="menu.name">
-      <i class="iconfont" :class="menu.icon"></i>
-    </router-link>
-  </div>
-  <div class="section">
-    <router-view></router-view>
+  <div class="flex flex-center">
+    <img src="@/assets/image/network-plus.png" alt="" srcset="" />
+    <span class="flex-1"
+      >Open DevTools and look for the <b>NetworkPlus</b> panel.</span
+    >
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useRouter } from "vue-router";
-import { useUserStore } from "@/store/user.js";
-
-const { newTodo } = useUserStore();
-
-const {
-  options: { routes },
-} = useRouter();
-
-const menus = computed(() => {
-  let res = [];
-  routes.forEach((item) => {
-    if (item.meta?.isMenu) {
-      res.push({
-        name: item.path.replace("/", ""),
-        path: item.path,
-        icon: item.meta?.icon,
-      });
-    }
-  });
-  return res;
-});
 </script>
 
 <style lang="scss" scoped>
-.section {
-  flex: 1;
-  padding: 0 10px;
-  overflow: hidden;
-  & > div {
-    height: 100%;
-    width: 100%;
-  }
-}
-.side-bar-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 5px;
-  a {
-    color: #333;
-    text-decoration: none;
-    margin: 8px 0;
-  }
-  .router-link-active {
-    color: var(--el-color-primary);
-  }
-  .logo {
-    width: 40px;
-    height: 40px;
-    margin-bottom: 10px;
-  }
-  .iconfont {
-    font-size: 22px;
-    margin: 5px 0;
-    padding: 2px;
-    cursor: pointer;
-    transition: all 0.2s;
-    &.active {
-      color: var(--el-color-primary);
-    }
-  }
-  border-right: 1px solid #dcdfe6;
+img {
+  width: 160px;
+  margin-right: 10px;
 }
 </style>
--- src/popup/main.js
+++ src/popup/main.js
@@ -1,21 +1,6 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import router from './router';
 import App from './App.vue';
-import copyToClipboard from '../directives/copyToClipboard';
 import '../assets/styles/index.scss';
 
-import VueJsonPretty from 'vue-json-pretty';
-import 'vue-json-pretty/lib/styles.css';
-
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-
-const pinia = createPinia();
 const app = createApp(App);
-app.component('VueJsonPretty', VueJsonPretty);
-app.directive('copyToClipboard', copyToClipboard);
-app.use(router).use(pinia).mount('#app');
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-}
+app.mount('#app');
