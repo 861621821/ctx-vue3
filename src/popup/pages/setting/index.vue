d402004115c2eaa8905af300e559258b87d34b10
liuwei <wei.liu@pingxx.com>
2024-02-26 14:18:40 +0800

    fix: 序列化报错、浏览器指纹、jira提醒

--- src/popup/pages/setting/index.vue
+++ src/popup/pages/setting/index.vue
@@ -9,7 +9,7 @@ import { ref } from 'vue';
 
 const enableJira = ref(true);
 chrome.storage.local.get('enableJira', res => {
-    enableJira.value = res.enableJira || true;
+    enableJira.value = res.enableJira ?? true;
 });
 
 const onChange = () => {
